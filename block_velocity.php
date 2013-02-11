f68611ef91ede0a2370040fefe50bb77c2838156
Tomasz Muras <nexor1984@gmail.com>
2013-02-11 17:07:51 +0100

    A bit of tidying up.

--- block_velocity.php
+++ block_velocity.php
@@ -4,7 +4,6 @@ require_once('velocity_form.php');
 
 class block_velocity extends block_base
 {
-
     public function init()
     {
         $this->title = get_string('velocity', 'block_velocity');
@@ -29,79 +28,84 @@ class block_velocity extends block_base
         $onemon = array();
         $week = array();
         $times = array();
-        $today = time();//date('l\, j F Y h:i A');
+        $today = time(); //date('l\, j F Y h:i A');
         $message = "";
         $option = "";
         $courses = array();
         $options = array('year' => '1 Year', 'sixmonths' => '6 Months', 'threemonths' => '3 Months', 'onemonth' => '1 Month', 'week' => '1 Week');
         $result = $DB->get_records_sql('SELECT id, course, timecompleted FROM {course_completions} WHERE userid = ? AND timecompleted > 0', array($userid));
-
-        if ($result) {
-            foreach ($result as $record) {
-                $course = $DB->get_record('course', array('id' => $record->course));
-                /*$info = new completion_info($course);
-                $completion = $info->get_completions($USER->id);*/
-                $time = $record->timecompleted;
-
-                array_push($times, $time);
-                $datediff = floor(($today - $time) / DAYSECS);
-                //var_dump($datediff); die();
-                $course_name = $course->fullname;
-                if ($datediff < 7) {
-                    array_push($week, $course_name);
-                    array_push($onemon, $course_name);
-                    array_push($threemon, $course_name);
-                    array_push($sixmon, $course_name);
-                    array_push($year, $course_name);
-                } else if ($datediff < 31) {
-                    array_push($onemon, $course_name);
-                } else if ($datediff < 93) {
-                    array_push($threemon, $course_name);
-                } else if ($datediff < 186) {
-                    array_push($sixmon, $course_name);
-                } else if ($datediff < 365) {
-                    array_push($year, $course_name);
-                } else {
-                    $messsage = "No Courses completed in given period of time";
-                }
-            }
-
-            $url = new moodle_url('',array('id'=>$COURSE->id));
-            $option = optional_param('velocity_time', 'year', PARAM_TEXT);
-            $choice = $options['year'];
-            if ($option) {
-                $choice = $option;
-            }
-            $select = new single_select($url, 'velocity_time', $options, $choice, array());
-            switch ($option) {
-                case "year":
-                    $courses = $year;
-                    break;
-                case "sixmonths" :
-                    $courses = $sixmon;
-                    break;
-                case "threemonths" :
-                    $courses = $threemon;
-                    break;
-                case "onemonth" :
-                    $courses = $onemon;
-                    break;
-                case "week" :
-                    $courses = $week;
-            }
-
-            if (empty($courses)) {
-                $message = "No Courses completed in given period of time";
-            } else {
-
-                foreach ($courses as $c) {
-                    $message = $message . $c . "<br/>";
-                }
-            }
-            $this->content->text = "Courses completed in the last <br/>" . $OUTPUT->render($select) . $message;
-        } else {
+        if (!$result) {
             $this->content->text = "You have no completed courses.";
+            return $this->content;
         }
+
+        foreach ($result as $record) {
+            $course = $DB->get_record('course', array('id' => $record->course));
+            /*$info = new completion_info($course);
+            $completion = $info->get_completions($USER->id);*/
+            $time = $record->timecompleted;
+
+            array_push($times, $time);
+            $datediff = floor(($today - $time) / DAYSECS);
+
+            $course_name = $course->fullname;
+            if ($datediff < 7) {
+                array_push($week, $course_name);
+                array_push($onemon, $course_name);
+                array_push($threemon, $course_name);
+                array_push($sixmon, $course_name);
+                array_push($year, $course_name);
+            } else if ($datediff < 31) {
+                array_push($onemon, $course_name);
+                array_push($threemon, $course_name);
+                array_push($sixmon, $course_name);
+                array_push($year, $course_name);
+            } else if ($datediff < 93) {
+                array_push($threemon, $course_name);
+                array_push($sixmon, $course_name);
+                array_push($year, $course_name);
+            } else if ($datediff < 186) {
+                array_push($sixmon, $course_name);
+                array_push($year, $course_name);
+            } else if ($datediff < 365) {
+                array_push($year, $course_name);
+            }
+        }
+
+        $url = new moodle_url('', array('id' => $COURSE->id));
+        $option = optional_param('velocity_time', 'year', PARAM_TEXT);
+        $choice = $options['year'];
+        if ($option) {
+            $choice = $option;
+        }
+        $select = new single_select($url, 'velocity_time', $options, $choice, array());
+        switch ($option) {
+            case "year":
+                $courses = $year;
+                break;
+            case "sixmonths" :
+                $courses = $sixmon;
+                break;
+            case "threemonths" :
+                $courses = $threemon;
+                break;
+            case "onemonth" :
+                $courses = $onemon;
+                break;
+            case "week" :
+                $courses = $week;
+        }
+
+        if (empty($courses)) {
+            $message = "No Courses completed in given period of time";
+        } else {
+
+            foreach ($courses as $c) {
+                $message = $message . $c . "<br/>";
+            }
+        }
+        $this->content->text = "Courses completed in the last <br/>" . $OUTPUT->render($select) . $message;
+
         return $this->content;
     }
 }
